1294aa67695807d92c5f9f73622e61296d59926f
gali <gbmatrix@gmail.com>
2019-01-11 23:30:16 +0200

    update logger

--- app.rb
+++ app.rb
@@ -1,12 +1,23 @@
 class Logger
 
+   def initialize
+     @f = File.open "log.txt", "a"
+   end
+   #class method
    def self.say_something
-        puts "Ha-Ha!"
+     puts "Ha-Ha!"
+   end
+
+   #instance method
+   def log_something logLine
+      @f.puts logLine
    end
 
 end #class
 
-
 #logger = Logger.new
 
 Logger.say_something
+
+logger = Logger.new
+logger.log_something "abra kadabra"
